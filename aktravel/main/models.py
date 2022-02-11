8b983c2eea96bcb760dcf87a5ab64e4f0ec844af
Afraiven <filip.antoniak99@gmail.com>
2022-02-11 09:38:03 +0100

    oferta

--- aktravel/main/models.py
+++ aktravel/main/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 class Oferta(models.Model):
-    name = models.CharField(max_length=200)
-    opis = models.CharField(max_length=200)
+    tytul = models.TextField(max_length=200)
+    historia = models.TextField(max_length=400, null=True, blank=True)
+    opis_dni = models.TextField(max_length=10000, null=True, blank=True)
+    termin = models.TextField(max_length=50, null=True, blank=True)
+    cena = models.CharField(max_length=50, null=True, blank=True)
+    cena_zawiera = models.TextField(max_length=1000, null=True, blank=True)
+    cena_nie_zawiera = models.TextField(max_length=1000, null=True, blank=True)
+    wazne_informacje = models.TextField(max_length=1000, null=True, blank=True)
+    uwagi = models.TextField(max_length=1000, null=True, blank=True)
     zdjecie_glowne = models.ImageField(null=True, blank=True)
-    def __str__(self):
-        return self.name
+    def str(self):
+        return self.tytul
     class Meta: 
         verbose_name = "Oferta"
         verbose_name_plural = "Oferty"
